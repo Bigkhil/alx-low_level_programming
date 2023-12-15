9b1c4585f55e075edd0680b67e6171a47f67d0cb
Mohamed khalil <khm40005000@outlook.com>
2023-12-15 23:35:48 +0200

    some changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int i = sizeof(int);
 	int c = sizeof(char);
 	int li = sizeof(long int);
 	int ll = sizeof(long long);
 	int f = sizeof(float);
+
 	printf("Size of a char: %d byte(s)\n", c);
 	printf("Size of a int: %d byte(s)\n", i);
 	printf("Size of a long int: %d byte(s)\n", li);

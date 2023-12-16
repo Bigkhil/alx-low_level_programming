b814cce39dc9f25771fe7b718582236cb5fa1d07
Mohamed khalil <khm40005000@outlook.com>
2023-12-16 03:41:14 +0200

    some changes

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	char x = 'a';
+
+	for (i = 0 ; i < 26 ; i++)
+	{
+		if (x == 'q' || x == 'e')
+		continue;
+		putchar (x);
+		x++;
+		}
+	putchar ('\n');
+	return (0);
+}

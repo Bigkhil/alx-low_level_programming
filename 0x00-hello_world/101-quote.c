aa62ababfe832ffb07cbc3a89124ddb1d5f38938
Mohamed khalil <khm40005000@outlook.com>
2023-12-16 17:58:55 +0200

    some changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,14 @@
 #define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
 #define SIZE sizeof(MESSAGE)
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void){
-	    if (write(STDERR_FILENO, MESSAGE, SIZE) != SIZE) {
-		            return 1;
-			        }
-	    putchar('\n');
-	        return 0;
+	if (write(STDERR_FILENO, MESSAGE, SIZE) != SIZE)
+		return 1;
+	return 0;
 }
 

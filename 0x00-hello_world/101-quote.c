0124697771d28ad5f5aa74f7f0cec499996f3223
Mohamed khalil <khm40005000@outlook.com>
2023-12-16 17:54:22 +0200

    some changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
-#define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+#define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
 #define SIZE sizeof(MESSAGE)
 
 int main(void){
 	    if (write(STDERR_FILENO, MESSAGE, SIZE) != SIZE) {
 		            return 1;
 			        }
+	    putchar('\n');
 	        return 0;
 }
 

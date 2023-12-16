05fdee7e9d7072dc2add6358665129fc887859cc
Mohamed khalil <khm40005000@outlook.com>
2023-12-16 18:04:26 +0200

    some changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void){
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59\n);
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59\n);
 	return (1);
 }

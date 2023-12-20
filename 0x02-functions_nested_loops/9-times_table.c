4bf004a91b3951e3e77108a76797efcf1383f0ce
Mohamed khalil <khm40005000@outlook.com>
2023-12-20 22:25:32 +0200

    some changes

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - prints the times table
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void times_table(void)
+{
+	char a, b;
+	int i, j, ans, x, z;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			ans = i * j;
+			x = ans % 10;
+			z = ans / 10;
+			a = x + '0';
+			b = z + '0';
+			if (ans > 9)
+			{
+				_putchar(b);
+				_putchar(a);
+			}
+			else
+			{
+				_putchar(b);
+			}
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}

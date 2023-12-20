6d1368e17757074ab82ece2e47ab19a3dfc5284e
Mohamed khalil <khm40005000@outlook.com>
2023-12-20 22:46:22 +0200

    some changes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - add two numbers
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int add(int x, int y)
+{
+	return (x+y);
+}

fa38d91daaea534c5f680b6e5102c3821839abce
Mohamed khalil <khm40005000@outlook.com>
2023-12-20 22:48:14 +0200

    some changes

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,12 +3,13 @@
 
 /**
  * add - add two numbers
- *
+ * @x: the number to read
+ * @y: the second number to read
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
 int add(int x, int y)
 {
-	return (x+y);
+	return (x + y);
 }

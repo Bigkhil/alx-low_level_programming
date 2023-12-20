b208bf949a3981fb6a46e047be7a61ef1e222a00
Mohamed khalil <khm40005000@outlook.com>
2023-12-20 17:11:04 +0200

    some changes

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - checks for integers
+ *@n: The number to read
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-');
+	return (0);
+}

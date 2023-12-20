04c4ff641135821d2e85451f2010cdb98ba28300
Mohamed khalil <khm40005000@outlook.com>
2023-12-20 17:17:01 +0200

    some changes

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - prints the absolute value of a number
+ *@n: The number to read
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _abs(int n)
+{
+	if(n < 0) return (-1 * n);
+	return (n);
+}

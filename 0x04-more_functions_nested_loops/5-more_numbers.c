71c699709f546ebb4c4a40a5894d7a2102a0120c
Mohamed khalil <khm40005000@outlook.com>
2024-01-02 17:03:06 +0200

    some changes

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+	char c;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 15 ; j++)
+		{
+			c = '0' + j;
+
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}

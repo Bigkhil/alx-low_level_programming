287fb5ffec0d8927d267b1ce6b14c52a4fa8b64d
Mohamed khalil <khm40005000@outlook.com>
2024-01-05 14:40:52 +0200

    some changes

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - update the value of variable to 98
+ *
+ * @str: pointer to variable
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int *ptr;
+
+	ptr = str;
+	while (*str != '\0')
+	{
+		str++;
+	}
+	while (ptr < str)
+	{
+		_putchar(*ptr);
+		ptr += 2;
+	}
+}

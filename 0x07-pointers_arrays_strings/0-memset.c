39fc6961daf130cb6ddd0946baf2d10f521928a2
Mohamed khalil <khm40005000@outlook.com>
2024-01-09 23:39:49 +0200

    some changes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
- * _memcpy - update the value of variable to 98
+ * _memset - update the value of variable to 98
  *
- * @dest: pointer to variable
- * @src: pointer to variable
+ * @s: pointer to variable
+ * @b: pointer to variable
  * @n: number to read
  * Return: pointer
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	char *ret;
 	unsigned int i;
 
-	ret = dest;
+	ret = s;
 	i = 0;
 	for (i = 0 ; i < n ; i++)
 	{
-		*dest = *src;
-		src++;
-		dest++;
+		*s = b;
+		s++;
 	}
 	return (ret);
 }

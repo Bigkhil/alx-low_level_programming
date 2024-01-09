52d2d263d06280957a5abef1710344f5e435a3c4
Mohamed khalil <khm40005000@outlook.com>
2024-01-10 00:49:19 +0200

    some changes

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,6 +9,9 @@
  */
 char *_strchr(char *s, char c)
 {
+	char *x;
+
+	x = 0;
 	while (*s != '\0')
 	{
 		if (*s == c)
@@ -17,5 +20,5 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	return (NULL);
+	return (x);
 }

778ab22a245ac1a2e9ddba4691fa2002855a9223
Mohamed khalil <khm40005000@outlook.com>
2024-01-16 22:58:31 +0200

    some changes

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - update the value of variable to 98
+ *
+ * @argc: number
+ * @argv: array of strings
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	int res, i;
+
+	res = 0;
+	if (argc <= 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1 ; i < argc ; i++)
+		{
+			if (checknumber(argv[i]) == 1)
+			{
+				res = res + atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", res);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/checknumber.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * checknumber - update the value of variable to 98
+ *
+ * @x: pointer
+ * Return: void
+ */
+int checknumber(char *x)
+{
+	while (*x != ' ' || *x != '\0')
+	{
+		if (*x < 48 || *x > 57)
+		{
+			return (0);
+		}
+		x++;
+	}
+	return (1);
+}

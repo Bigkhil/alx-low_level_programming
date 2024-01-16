654cee4e102def5ba412a8b8930fd043473fb07b
Mohamed khalil <khm40005000@outlook.com>
2024-01-16 22:28:20 +0200

    some changes

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - update the value of variable to 98
+ *
+ * @argc: number
+ * @argv: array of strings
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv[argc];
+	printf("%d\n", argc);
+	return (0);
+}

997ea08a155e9c8964807f953102e8a1b7171503
Mohamed khalil <khm40005000@outlook.com>
2024-01-25 01:23:04 +0200

    some changes

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,14 +14,14 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	arr = (int **)malloc(sizeof(int *) * width);
+	arr = (int **)malloc(sizeof(int *) * height);
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0 ; i < width ; i++)
 	{
-		arr[i] = (int *)malloc(sizeof(int) * height);
+		arr[i] = (int *)malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
 			return (NULL);

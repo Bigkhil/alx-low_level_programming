8458b0b99e98ac83939c9a5560f85b6ebfd0e309
Mohamed khalil <khm40005000@outlook.com>
2024-01-24 21:50:32 +0200

    some changes

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strdup - function does some behavior
+ *
+ * @str: number
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+	char *arr, *temp1, *temp2;
+	int i, size;
+
+	size = 1;
+	temp1 = str;
+	while (*str != '\0')
+	{
+		size++;
+		str++;
+	}
+	arr = malloc(sizeof(char) * size);
+	temp2 = arr;
+	if (str == NULL || arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < size ; i++, temp1++)
+	{
+		arr[i] = *temp1;
+	}
+	return (temp2);
+}

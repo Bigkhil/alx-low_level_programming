7d67aad015695070731e21631f1bd0ca010313e5
Mohamed khalil <khm40005000@outlook.com>
2024-01-26 13:08:12 +0200

    some changes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,19 +9,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *y;
 	unsigned int i;
-	void *x, *temp;
+	void *x;
 
 	x = malloc(nmemb * size);
 	if (x == NULL || nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	temp = x;
 	y = x;
-	while (y != temp + (size * nmemb) - 1)
+	for (i = 0 ; i < (nmemb * size) ; i++)
 	{
-		*y = 0;
-		y++;
+		y[i] = 0;
 	}
-	return (temp);
+	return (x);
 }

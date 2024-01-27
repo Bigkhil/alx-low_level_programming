6cc3a4432ff845de8b7ecab598bfa8e7cb510097
Mohamed khalil <khm40005000@outlook.com>
2024-01-27 16:10:30 +0200

    some changes

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+ * int_index - function does some behavior
+ * @array: string
+ * @size: size_t
+ * @cmp: pointer to function
+ * Return: void
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	size_t i;
+
+	if (array && size > 0 && cmp)
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}

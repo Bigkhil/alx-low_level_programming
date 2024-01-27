4c3b1625216e1cf0a1a2aab2f4e05c4bbeaf9ce3
Mohamed khalil <khm40005000@outlook.com>
2024-01-27 15:59:18 +0200

    some changes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,7 +2,8 @@
 #define EXAMPLE_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
 #endif

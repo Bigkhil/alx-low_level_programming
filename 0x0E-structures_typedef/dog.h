6e1ae94d5d2bcaece648bc0418c71bb377ee5383
Mohamed khalil <khm40005000@outlook.com>
2024-01-29 15:55:47 +0200

    some changes

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int _putchar(char c);
+/**
+ * struct dog - function does some behavior
+ * @name: string
+ * @age: float
+ * @owner: string
+ * Description: void
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

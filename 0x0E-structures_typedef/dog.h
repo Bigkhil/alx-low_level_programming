05162a0c5e4b9e682016bc7653e62b63d2f5679c
Mohamed khalil <khm40005000@outlook.com>
2024-01-29 16:37:49 +0200

    some changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,12 +11,12 @@ int _putchar(char c);
  * @owner: string
  * Description: void
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif

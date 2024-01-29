5d67f3605808b7af869a4a83de245a77ca6f280f
Mohamed khalil <khm40005000@outlook.com>
2024-01-29 21:33:31 +0200

    some changes

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+/**
+ * free_dog - function does some behavior
+ * @d: pointer to struct variable
+ * Return: pointer
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+	free(d->name);
+	free(d->owner);
+}

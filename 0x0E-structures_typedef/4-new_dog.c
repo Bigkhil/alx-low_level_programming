802eafa63043073aef48bda8615be54fa7f95c84
Mohamed khalil <khm40005000@outlook.com>
2024-01-29 21:26:38 +0200

    some changes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -47,7 +47,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (d == NULL || !name || age < 0 || !owner)
 	{
-		free(d);
 		return (NULL);
 	}
 	d->name = malloc(sizeof(char) * _strlen(name));

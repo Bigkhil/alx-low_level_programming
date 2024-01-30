e9ec14547b6243d79b37641f6eaed403387f0332
Mohamed khalil <khm40005000@outlook.com>
2024-01-30 21:29:32 +0200

    some changes

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,9 +14,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][0] != '+' &&
-	 argv[2][0] != '-' &&
-	  argv[2][0] != '*' && argv[2][0] != '/' && argv[2][0] != '%')
+	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);

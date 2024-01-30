efe7d62c21d2b5bb1d5837e7e8a7b898aea3c28b
Mohamed khalil <khm40005000@outlook.com>
2024-01-30 17:59:51 +0200

    some changes

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,50 @@
+#include "3-calc.h"
+/**
+ * main - function does some behavior
+ * @argc: arguments counter
+ * @argv: array of pointer to characters
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	char *error = "Error";
+	int x, y, i;
+
+	if (argc != 4)
+	{
+		i = 0;
+		while (i < 5)
+		{
+			_putchar(error[i]);
+			i++;
+		}
+		_putchar('\n');
+		exit(98);
+	}
+	if (argv[2] != '+' &&
+	 argv[2] != '-' && argv[2] != '*' && argv[2] != '/' && argv[2] != '%')
+	{
+		i = 0;
+		while (i < 5)
+		{
+			_putchar(error[i]);
+			i++;
+		}
+		_putchar('\n');
+		exit(99);
+	}
+	if ((argv[2] == '/' || argv[2] == '%') && (argv[3] == '0'))
+	{
+		i = 0;
+		while (i < 5)
+		{
+			_putchar(error[i]);
+			i++;
+		}
+		_putchar('\n');
+		exit(100);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+	return (get_op_func(argv[2])(x, y));
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - function does some behavior
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function does some behavior
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function does some behavior
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function does some behavior
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function does some behavior
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

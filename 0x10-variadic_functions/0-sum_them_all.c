a9c5081dd1b0940fab2acc139b93cad9b2ff6aa5
Mohamed khalil <khm40005000@outlook.com>
2024-01-31 22:23:35 +0200

    some changes

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,14 +6,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-	{
-		return (0);
-	}
 	va_list x;
 	unsigned int i;
 	int sum;
 
+	if (n == 0)
+	{
+		return (0);
+	}
 	va_start(x, n);
 	sum = 0;
 	for (i = 0 ; i < n ; i++)

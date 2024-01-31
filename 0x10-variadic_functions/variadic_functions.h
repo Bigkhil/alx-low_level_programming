2628b56c6fc061e5391fba8c9cd9daf4740c5c49
Mohamed khalil <khm40005000@outlook.com>
2024-01-31 22:22:19 +0200

    some changes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,8 @@
-#ifndef EXAMPLE_H
-#define EXAMPLE_H
+#ifndef variadic_functions_H
+#define variadic_functions_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 int sum_them_all(const unsigned int n, ...);
 #endif

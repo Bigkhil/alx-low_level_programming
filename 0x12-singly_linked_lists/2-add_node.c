cc73696ae780f1fd9cb1d4bb83c9b337cd4465fc
Mohamed khalil <khm40005000@outlook.com>
2024-02-04 16:33:45 +0200

    some changes

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * add_node - function to add node to the beginning of the list
+ * @head: pointer to head
+ * @str: string
+ * Return: pointer
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->next = *head;
+	*head = &new;
+	new->str = strdup(str);
+	return (&new);
+}

24464005bebb6d75591b3cd4ce8b802390ebb746
Mohamed khalil <khm40005000@outlook.com>
2024-02-06 20:21:22 +0200

    some changes

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * pop_listint - function to delete head node
+ * @head: pointer to pointer
+ * Return: int
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *curr = *head;
+	int x = (*head)->n;
+
+	if (!(*head))
+	{
+		return (0);
+	}
+	free(*head);
+	*head = (*head)->next;
+	return (x);
+}

b2c6ec2de5442588f0ca1bb85661ab48ee02e004
Mohamed khalil <khm40005000@outlook.com>
2024-02-07 00:32:21 +0200

    some changes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,6 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (new == NULL || (*head == NULL && idx != 0))
 	{
+		free(new);
 		return (NULL);
 	}
 	new->n = n;

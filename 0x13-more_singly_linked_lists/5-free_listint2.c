8e52c9b20c88404de87290b50bde2e85f6f9c855
Mohamed khalil <khm40005000@outlook.com>
2024-02-06 17:08:19 +0200

    some changes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,14 +8,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
 
-	if (*head != NULL)
-	{
-		while (temp->next != NULL)
+		while (*head)
 		{
 			temp = (*head)->next;
 			free(*head);
 			*head = temp;
 		}
-		free(*head);
-	}
 }

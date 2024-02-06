1e634ed22c595213c2a3a2eb7345ef81aa9080ea
Mohamed khalil <khm40005000@outlook.com>
2024-02-06 16:35:00 +0200

    some changes

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,8 +16,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 	new->n = n;
 	new->next = NULL;
-	if (curr == NULL)
-		curr = new;
+	if (*head == NULL)
+		*head = new;
 	else
 	{
 		while (curr->next != NULL)

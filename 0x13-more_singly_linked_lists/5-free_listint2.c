b26e4270a8b39873921cb697775a1e5247fcb73d
Mohamed khalil <khm40005000@outlook.com>
2024-02-06 16:48:52 +0200

    some changes

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - function to free linked list
+ * @head: pointer to pointer
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	if (*head != NULL)
+	{
+		while (temp->next != NULL)
+		{
+			temp = (*head)->next;
+			free(*head);
+			*head = temp;
+		}
+		free(*head);
+		*head = NULL;
+	}
+}

729651535679d5e481e1c22b1f478ca6d0bb35e5
Mohamed khalil <khm40005000@outlook.com>
2024-02-08 23:16:55 +0200

    some changes

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_bit - function to convert decimal to binary
+ * @n: unsigned long int
+ * @index: unsigned int
+ * Return: int
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(*n) * 8)
+		return (-1);
+	*n = *n | (1 << index);
+	return (1);
+}

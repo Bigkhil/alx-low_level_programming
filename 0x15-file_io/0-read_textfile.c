3f338e22693fc38c7f5c676ce21e17f032a26970
Mohamed khalil <khm40005000@outlook.com>
2024-02-13 17:39:44 +0200

    some changes

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * read_textfile - function does something
+ * @filename: string
+ * @letters: size_t
+ * Return: ssize_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char buffer[letters];
+	int file;
+	ssize_t bytes;
+
+	if (filename == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	bytes = read(file, buffer, letters);
+	if (bytes == -1)
+		return (0);
+	return (bytes);
+}

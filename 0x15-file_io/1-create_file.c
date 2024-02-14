5d42851b3fbf4b00d6004ce4e9bb432526ef7c77
Mohamed khalil <khm40005000@outlook.com>
2024-02-14 23:19:24 +0200

    some changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	mode_t permissions = S_IRUSR | S_IWUSR;
-	int file, written;
+	int file, written = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,8 +22,13 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (file == -1)
 		return (-1);
-	written = write(file, text_content, strlen(text_content));
-	if (written <= 0)
+	if (text_content)
+		written = write(file, text_content, strlen(text_content));
+	else
+	{
+		file = open(filename, O_WRONLY | O_TRUNC);
+	}
+	if (written == -1)
 		return (-1);
 	close(file);
 	return (1);

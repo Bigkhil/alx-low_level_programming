98f927a7cc3e0a0d24436bf3374e14f1edb0578f
Mohamed khalil <khm40005000@outlook.com>
2024-02-15 20:34:25 +0200

    some changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -45,15 +45,5 @@ int main(int argc, char **argv)
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	if (close(file1) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", file1);
-		exit(100);
-	}
-	if (close(file2) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", file2);
-		exit(100);
-	}
 	return (0);
 }

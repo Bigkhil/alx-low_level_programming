b128169d2332fa92cb2c3405fd8bfbdb9ae0e5d6
Mohamed khalil <khm40005000@outlook.com>
2024-03-05 23:31:23 +0200

    some changes

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: number of arguments
+ * @argv: pointer to array of strings
+ * Return: Always 0.
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

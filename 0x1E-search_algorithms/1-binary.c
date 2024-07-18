fa754d81a561131544030260345be48700cc7aab
Mohamed khalil <khm40005000@outlook.com>
2024-07-18 04:24:36 +0300

    some changes

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,7 +12,7 @@ int binary_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	while(l <= r)
+	while (l <= r)
 	{
 		mid = (l + r) / 2;
 		printf("Searching in array: ");
@@ -28,10 +28,7 @@ int binary_search(int *array, size_t size, int value)
 		else if (value < array[mid])
 			r = mid - 1;
 		else
-		{
-			printf("Found %d at index: %ld\n", value, mid);
 			return (mid);
-		}
 	}
 	return (-1);
 }
